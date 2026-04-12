8c27368c319701c5a5ba6104fa0dbd282025a704
NriotHrreion <nriot233@outlook.com>
2026-04-12 21:59:52 +0800

    feat: Add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import { Footer } from "./footer";
 import LogoIcon from "@/assets/logo.png";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { googleSansCode, notoSansSC } from "@/lib/fonts";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export const metadata: Metadata = {
   title: "OPanel 资源库",
@@ -17,10 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="zh-cn">
+    <html lang="zh-cn" suppressHydrationWarning>
       <body className={cn("flex flex-col justify-center items-center w-screen h-screen antialiased", notoSansSC.className, googleSansCode.variable)}>
-        {children}
-        <Footer />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem>
+          {children}
+          <Footer />
+          <ThemeToggle />
+        </ThemeProvider>
       </body>
     </html>
   );
